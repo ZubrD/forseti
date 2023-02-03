d398ea04cb1afe252924ebf32b73b7f15eb5a4b2
Денис Зубрицкий <fort_zd@mail.ru>
2023-02-03 12:17:51 +0700

    Перед переводом промиса в сервере на async

--- server/ruleModel.js
+++ server/ruleModel.js
@@ -10,7 +10,8 @@ const pool = new Pool({
 const getRule = () => {
   return new Promise(function (resolve, reject) {
     pool.query(
-      "SELECT * FROM public.forseti_rules ORDER BY id ASC LIMIT 10",
+      "SELECT * FROM public.forseti_rules WHERE rule_number='987674-7' ORDER BY id ASC LIMIT 100"
+      /* "SELECT * FROM public.forseti_rules ORDER BY id ASC LIMIT 20" */,
       (error, results) => {
         if (error) {
           reject(error);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.css";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App";
 import "./index.css";
 import { createStore } from "./store/createStore";
@@ -11,11 +13,11 @@ const store = createStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
         <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+      </Provider>
+    </React.StrictMode>
+  </BrowserRouter>
 );
